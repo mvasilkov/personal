1875d3be841ef866c501c042ece8806961636476
Mark Vasilkov <mvasilkov@gmail.com>
2022-04-22 21:39:07 +0300

    Update

--- xbuild/templatetags.py
+++ xbuild/templatetags.py
@@ -1,3 +1,13 @@
 from django import template
 
 register = template.Library()
+
+URLS = {
+    'steamdb_app': 'https://steamdb.info/app/%d/',
+    'steam_run': 'steam://rungameid/%d',
+}
+
+
+@register.simple_tag
+def absolute_url(url_name, appid):
+    return URLS[url_name] % appid
